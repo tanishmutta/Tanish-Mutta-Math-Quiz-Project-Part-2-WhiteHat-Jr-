0c1db04016c1938c1a46b701b5809fa3a93b8d2a
tanishmutta <98214255+tanishmutta@users.noreply.github.com>
2022-10-18 20:27:43 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,8 @@
+function login() {
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("Player1_name" , player1_name);
+    localStorage.setItem("Player2_name" , player2_name);
+    window.location = "quiz_game_page.html";
+}
